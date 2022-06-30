10c4e3d931c18438e444ef17449e2cd62a0a2839
Dan Wagner <danwagnerwd@gmail.com>
2022-06-29 23:53:18 -0400

    update reference for top and right position for modal configuration header to allow remove animation. Create first step to start modal configuration

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 class PageVisit {
   #lastScrollTop = 0;
+  #windowHeight = window.innerHeight;
+  #containers = [document.querySelector('#intro-header'), document.querySelector('#container-modal-configuration')]
 
   init() {
-    const windowHeight = window.innerHeight;
-    const containers = [document.querySelector('#intro-header'), document.querySelector('#container-modal-configuration')];
-    this.#sizeContainers(containers, windowHeight)
-    this.#addWindowListener(containers, windowHeight);
+    this.#sizeContainers(this.#containers, this.#windowHeight)
+    this.#addWindowListener(this.#containers, this.#windowHeight);
   }
 
   #addWindowListener(containers, windowHeight) {
@@ -18,58 +18,32 @@ class PageVisit {
   }
 
   #fadeBackground(windowHeight) {
-    document.addEventListener('scroll', () => {
+    const myScrollEvent = () => {
       if (window.pageYOffset > 20) {
         const opacity = (window.pageYOffset / windowHeight).toString();
         document.body.style.background = `linear-gradient(rgba(245, 245, 245, ${opacity}), rgba(245, 245, 245, ${opacity})), rgba(14, 115, 179, 1)`;
       } else {
         document.body.style.background = `rgba(14, 115, 179, 1)`;
       }
-      this.#controlModalPlacement();
-    });
+      this.#controlModalPlacement(myScrollEvent);
+    }
+    document.addEventListener('scroll', myScrollEvent);
   }
 
-  #controlModalPlacement() {
+  #controlModalPlacement(myScrollEvent) {
     const distance = window.scrollY;
     const modalContainer = document.querySelector('#container-modal-configuration');
     const modalHeading = document.querySelector('#modal-configuration-header');
     const modal = document.querySelector('#modal-configuration');
     const bounding = modalContainer.getBoundingClientRect();
     const scrollTopDistance = window.pageYOffset || document.documentElement.scrollTop;
-    if (bounding.top >= 0) {
-      modal.style.transform = `translateY(${distance * 0.3}px)`;
-    } else {
-      modal.style.transform = modal.style.transform;
-    }
-    if (bounding.top <= 400 &&
-      bounding.top >= 0 &&
-      bounding.left >= 0 &&
-      bounding.right <= (window.innerWidth || document.documentElement.clientWidth)) {
+    bounding.top >= 0 ? modal.style.transform = `translateY(${distance * 0.3}px)` : modal.style.transform = modal.style.transform;
+    if (bounding.top <= 400 && bounding.top >= 0 && bounding.left >= 0 && bounding.right <= (window.innerWidth || document.documentElement.clientWidth)) {
       if (scrollTopDistance > this.#lastScrollTop) {
+        this.#showContent(modal, modalContainer, modalHeading, myScrollEvent);
         modalContainer.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
-        this.#changeDisplay([modalHeading], 'block');
-        new Promise(resolve => {
-          setTimeout(resolve, 300);
-        }).then(() => {
-          this.#changeOpacity([modalHeading], '1');
-          this.#startBuildDemo(modal, modalContainer);
-        }).then(() => {
-          new Promise(resolve => {
-            setTimeout(resolve, 300);
-          }).then(() => {
-            this.#addPulseAnimation(modal, modalHeading);
-          });
-        });
       } else {
-        this.#changeOpacity([modalHeading], '0');
-        this.#removePulseAnimation(modal, modalHeading);
-        this.#cancelBuildDemo(modal, modalContainer);
-        new Promise(resolve => {
-          setTimeout(resolve, 300);
-        }).then(() => {
-          this.#changeDisplay([modalHeading], 'none');
-          this.#cancelModalDemo(modal);
-        });
+        this.#removeContent(modal, modalContainer, modalHeading);
       }
       this.#lastScrollTop = scrollTopDistance <= 0 ? 0 : scrollTopDistance;
     }
@@ -78,6 +52,34 @@ class PageVisit {
   #sizeContainers(containers, height) {
     containers.forEach(element => {
       element.style.height = `${height}px`;
+    });
+  }
+
+  #showContent(modal, modalContainer, modalHeading, myScrollEvent) {
+    this.#changeHeadingDisplay([modalHeading], 'block', modal.getBoundingClientRect().y, modalContainer.style.height);
+    new Promise(resolve => {
+      setTimeout(resolve, 200);
+    }).then(() => {
+      this.#changeOpacity([modalHeading], '1');
+      this.#startBuildDemo(modal, modalContainer, modalHeading, myScrollEvent);
+    }).then(() => {
+      new Promise(resolve => {
+        setTimeout(resolve, 200);
+      }).then(() => {
+        this.#addPulseAnimation(modal, modalHeading);
+      });
+    });
+  }
+
+  #removeContent(modal, modalContainer, modalHeading) {
+    this.#changeOpacity([modalHeading], '0');
+    this.#removePulseAnimation(modal, modalHeading);
+    this.#cancelBuildDemo(modal, modalContainer);
+    new Promise(resolve => {
+      setTimeout(resolve, 200);
+    }).then(() => {
+      this.#changeHeadingDisplay([modalHeading], 'none');
+      this.#cancelModalDemo(modal);
     });
   }
 
@@ -93,10 +95,16 @@ class PageVisit {
     }
   }
 
-  #changeDisplay(elements, display) {
+  #changeHeadingDisplay(elements, display, offsetSibling, offsetHeight) {
+    const heightLength = offsetHeight.length - 2;
+    const validString = parseInt(offsetHeight.slice(0, heightLength));
+    const topPosition = `${(validString + offsetSibling) - 140}px`;
+    const leftPosition = `${(window.innerWidth / 2) + 20}px`;
     if (display === 'block') {
       elements.forEach((element) => {
         element.style.display = 'block';
+        element.style.top = topPosition;
+        element.style.right = leftPosition;
       });
     } else {
       elements.forEach((element) => {
@@ -105,16 +113,22 @@ class PageVisit {
     }
   }
 
-  #startBuildDemo(modal, modalContainer) {
+  #startBuildDemo(modal, modalContainer, header, myScrollEvent) {
     modalContainer.style.background = 'rgb(207, 207, 207)';
     modal.classList.add('special-hover');
     modal.style.width = '700px';
     modal.style.height = '400px';
+    modal.addEventListener('click', () => {
+      document.removeEventListener('scroll', myScrollEvent);
+      this.#removePulseAnimation(modal, header);
+      this.#cancelBuildDemo(modal, modalContainer);
+      document.querySelector('#intro-header').style.display = 'none';
+      header.style.display = 'none';
+    });
   }
 
-  #addPulseAnimation(modal, header) {
-    modal.style.animation = 'pulse 2s ease-out infinite';
-    header.style.animation = 'pulseHeading 2s ease-out infinite'
+  #addPulseAnimation(modal) {
+    modal.style.animation = 'pulse 1.5s ease-out infinite';
   }
 
   #cancelBuildDemo(modal, modalContainer) {
@@ -127,9 +141,8 @@ class PageVisit {
     modal.style.height = '350px';
   }
 
-  #removePulseAnimation(modal, header) {
+  #removePulseAnimation(modal) {
     modal.style.animation = 'none';
-    header.style.animation = 'none';
   }
 }
 
